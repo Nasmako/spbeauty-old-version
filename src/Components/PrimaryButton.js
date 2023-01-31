5b41495a9caf2985f2bb5e485b0cd4e28e98882c
Nasmako <123558513+Nasmako@users.noreply.github.com>
2023-01-31 19:57:31 +0000

    change buttons

--- src/Components/PrimaryButton.js
+++ src/Components/PrimaryButton.js
@@ -5,18 +5,20 @@ import styled from "styled-components";
 
 function PrimaryButton(props) {
     // Здесь устанавливается минимальная ширина для кнопки
-    let minWidth = props.minWidth === undefined ? '144px' : props.minWidth; 
+    let minWidth = props.minWidth === undefined ? '217px' : props.minWidth; 
     const Button = styled.div`
-        background: #4B759E;
-        border: 1px solid #152842;
+        background: #F8FAFC;
+        border-radius: 24px;
+        border: 1px solid #E2E8F0;
         box-shadow: inset 0px 1px 2px rgba(10, 15, 26, 0.08);
         min-width: ${minWidth};
-        color: white;
+        color: #64748B;
         &:hover {
-            background: linear-gradient(180deg, #4D9FF0 0%, #36699D 100%);
+            background: linear-gradient(180deg, #DFEAF1 0%, #F8FAFC 80%);
         }
         &:active {
-            background: #152842;
+            background: #3B82F6;
+            color: white;
         }
     `;
     return (
